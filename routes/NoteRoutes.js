1a12e61e137f7c6a0678c112af1eb919bcc6df2c
System Administrator <110475574+Pablineee@users.noreply.github.com>
2024-10-11 16:13:29 -0400

    All endpoints implemented and tested.

--- routes/NoteRoutes.js
+++ routes/NoteRoutes.js
@@ -1,60 +1,152 @@
-const noteModel = require('../models/Notes.js');
+const Note = require('../models/NotesModel');
+const express = require('express');
+const router = express.Router();
+
 //TODO - Create a new Note
 //http://mongoosejs.com/docs/api.html#document_Document-save
-app.post('/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     // Validate request
-    if(!req.body.content) {
+    const { noteTitle, noteDescription, priority } = req.body;
+    if(!noteTitle || !noteDescription || !priority) {
         return res.status(400).send({
             message: "Note content can not be empty"
         });
     }
     //TODO - Write your code here to save the note
+
+    const newNote = new Note({
+        noteTitle: noteTitle,
+        noteDescription: noteDescription,
+        priority: priority
+    });
+
+    newNote.save((err, result) => {
+        if (err){
+            console.error(err);
+        } else {
+            console.log(result);
+        }
+    });
+
+    res.status(200).send({
+        message: "Note successfully created.",
+        note: newNote
+    });
 });
 
 //TODO - Retrieve all Notes
 //http://mongoosejs.com/docs/api.html#find_find
-app.get('/notes', (req, res) => {
+router.get('/notes', async (req, res) => {
     // Validate request
-    if(!req.body.content) {
-        return res.status(400).send({
-            message: "Note content can not be empty"
+    // if(!req.body.content) {
+    //     return res.status(400).send({
+    //         message: "Note content can not be empty"
+    //     });
+    // }
+    //TODO - Write your code here to returns all note
+
+    try {
+        const notes = await Note.find();
+        res.status(200).send(notes);
+    } catch (err){
+        res.status(500).send({
+            message: err.message
         });
     }
-    //TODO - Write your code here to returns all note
 });
 
 //TODO - Retrieve a single Note with noteId
 //http://mongoosejs.com/docs/api.html#findbyid_findById
-app.get('/notes/:noteId', (req, res) => {
+router.get('/notes/:noteId', async (req, res) => {
     // Validate request
-    if(!req.body.content) {
+    const { noteId } = req.params;
+    if(!noteId) {
         return res.status(400).send({
             message: "Note content can not be empty"
         });
     }
     //TODO - Write your code here to return onlt one note using noteid
+
+    try {
+        const note = await Note.findById(noteId);
+        res.status(200).send(note);
+    } catch (error){
+        res.status(500).send({
+            message: error.message
+        });
+    }
 });
 
 //TODO - Update a Note with noteId
 //http://mongoosejs.com/docs/api.html#findbyidandupdate_findByIdAndUpdate
-app.put('/notes/:noteId', (req, res) => {
-    // Validate request
-    if(!req.body.content) {
+router.put('/notes/:noteId', async (req, res) => {
+    
+    const { noteId } = req.params;
+
+    const note = await Note.findById(noteId);
+
+    if (!note){
+        res.status(500).send({ message: "Note not found." });
+    }
+
+    const { noteTitle, noteDescription, priority } = req.body;
+
+    if(!noteId || !noteTitle || !noteDescription || !priority) {
         return res.status(400).send({
             message: "Note content can not be empty"
         });
     }
     //TODO - Write your code here to update the note using noteid
+
+    try {
+        const updatedNote = await Note.findByIdAndUpdate(
+            noteId,
+            {
+                noteTitle,
+                noteDescription,
+                priority
+            }
+        );
+        if (updatedNote){
+            const note = await Note.findById(noteId);
+            res.status(200).send({
+                message: "Note successfully updated.",
+                note: note
+            });
+        }
+    } catch(error){
+        res.status(500).send({
+            message: error.message 
+        });
+    }
+
 });
 
 //TODO - Delete a Note with noteId
 //http://mongoosejs.com/docs/api.html#findbyidandremove_findByIdAndRemove
-app.delete('/notes/:noteId', (req, res) => {
+router.delete('/notes/:noteId', async (req, res) => {
     // Validate request
-    if(!req.body.content) {
-        return res.status(400).send({
-            message: "Note content can not be empty"
-        });
+
+    const { noteId } = req.params;
+
+    if (!noteId){
+        res.status(404).send({ message: "Invalid ID" });
     }
     //TODO - Write your code here to delete the note using noteid
+
+    try {
+        const deletedNote = await Note.findByIdAndDelete(noteId);
+        if (deletedNote){
+            res.status(200).send({
+                message: "Note successfully deleted.",
+                deleted: deletedNote
+            });
+        }
+    } catch(err){
+        res.status(500).send({ message: err.message });
+    }
+
+
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const routes = require('./routes/NoteRoutes');
 const PORT = process.env.PORT || 8081;
 
-const DB_URL = 'mongodb+srv://admin:KR3bmfSEL47MsASQ@cluster0.tpsk6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const DB_URL = 'mongodb+srv://admin:KR3bmfSEL47MsASQ@cluster0.tpsk6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.Promise = global.Promise;
 
@@ -21,11 +24,11 @@ mongoose.connect(DB_URL, {
     process.exit();
 });
 
-
 app.get('/', (req, res) => {
     res.send("<h1>Welcome to Note taking application - Week06 Exercise</h1>");
 });
 
+app.use('/', routes);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
